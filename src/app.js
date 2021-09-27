ce1cb442e5a356199420eb74b74d77100640e293
prakalpshakya31 <prakalpshakya31@gmail.com>
2021-09-27 15:11:39 +0530

    get request

--- src/app.js
+++ src/app.js
@@ -11,16 +11,52 @@ app.get('/', (req, res) => {
 })
 
 // create a new student
-app.post('/students', (req, res) => {
-  const user = new Student(req.body)
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user)
-    })
-    .catch((e) => {
-      res.status(400).send(e)
-    })
+// app.post('/students', (req, res) => {
+//   const user = new Student(req.body)
+//   user
+//     .save()
+//     .then(() => {
+//       res.status(201).send(user)
+//     })
+//     .catch((e) => {
+//       res.status(400).send(e)
+//     })
+// })
+
+app.post('/students', async (req, res) => {
+  try {
+    const user = new Student(req.body)
+    const createUser = await user.save()
+    res.status(201).send(createUser)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+// read the data of all students
+app.get('/students', async (req, res) => {
+  try {
+    const studentsData = await Student.find()
+    res.send(studentsData)
+  } catch (e) {
+    res.send(e)
+  }
+})
+
+// get the individual student using id
+app.get('/students/:id', async (req, res) => {
+  try {
+    const studentData = await Student.findById(
+      req.params.id
+    )
+    if (!studentData) {
+      return res.status(404).send()
+    } else {
+      res.send(studentData)
+    }
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
 app.listen(port, () => {
